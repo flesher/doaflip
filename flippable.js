3fc315d6dc272f38a0887ff8b85d139a43283d3b
Matt Flesher <matt.flesher@gmail.com>
2013-07-18 20:21:29 -0600

    functional

--- flippable.js
+++ flippable.js
@@ -1,13 +1,21 @@
 (function ( $ ) {
 	
-	$.fn.flippable = function( options ) {
+	$.fn.underflip = function( options ) {
 		var self = this;
 
-		var height  = $(this).height();
+		var height  = $(this).height(),
+				animation = false,
+		    animationstring = 'animation',
+		    keyframeprefix = '',
+		    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+		    pfx  = '',
+		    settings,
+		    initRot,
+		    endRot;
 
-    var settings = $.extend({
-	    init      : 'closed',
-	    trigger   : ' ',
+    settings = $.extend({
+	    init      : 'open',
+	    trigger   : 'click',
 	    direction : ' ',
 	    speed     : ' ',
 	    ease      : ' ',
@@ -15,58 +23,74 @@
 	    rotation  : ' '
     }, options );
 
-		//set intial css according to init and dir
-		self.css({
-			'-webkit-transform': 'perspective( 600px ) rotateX( 0deg )',
-			'-webkit-transform-origin': '50% 0%',
-			'-webkit-transition': 'opacity 1s ease-in'
-		});
+    //sets initial rotation based on init setting
+    if (settings.init == 'open') initRot = 0;
+    else if (settings.init == 'closed') initRot = 90;
 
-		// if( animation === false ) {
 		 
-		//   // animate in JavaScript fallback
+		//checks if animation requires vendor prefix
+		if( self[0].style.animationName ) animation = true;     
+		
+		//if so we'll throw on the prefixes 
+		if( animation === false ) {
+		  for( var i = 0; i < domPrefixes.length; i++ ) {
+		    if( self[0].style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+		      pfx = domPrefixes[ i ];
+		      animationstring = pfx + 'Animation';
+		      keyframeprefix = '-' + pfx.toLowerCase() + '-';
+		      animation = true;
+		      break;
+		    }
+		  }
+		}
+
+		if( animation === false ) {
 		 
-		// } else {
-		//   // elm.style[ animationstring ] = 'rotate 1s linear infinite';
+		  // animate in JavaScript fallback
 		 
-		  var keyframes = '@-webkit-keyframes swing { ' +
-		                    'from {-webkit-transform: perspective( 600px ) rotateX( 0deg );}'+
-		                    'to {-webkit-transform: perspective( 600px ) rotateX( -90deg );}'+
+		} else {
+			// define the css keyframe
+			var keyframes = '@'+ keyframeprefix + 'keyframes swing { ' +
+		                    'from {'+ keyframeprefix + 'transform: perspective( 600px ) rotateX( 0deg );}'+
+		                    'to {'+ keyframeprefix + 'transform: perspective( 600px ) rotateX( -90deg );}'+
 		                  '}';
 
+		 	// define the class of how we want the element to end up after animation
 			var after     = '.after { ' +
-											  '-webkit-animation-timing-function: ease-in;' +
-											  '-webkit-animation-fill-mode: forwards;' +
-											  '-webkit-animation-name: swing;' +
-											  '-webkit-animation-duration: 0.5s;' +
+											  ''+ keyframeprefix + 'animation-timing-function: ease-in;' +
+											  ''+ keyframeprefix + 'animation-fill-mode: forwards;' +
+											  ''+ keyframeprefix + 'animation-name: swing;' +
+											  ''+ keyframeprefix + 'animation-duration: 0.5s;' +
 											'}';
-
-		  console.log(keyframes);
-		 
+ 
 		  if( document.styleSheets && document.styleSheets.length ) {
-		 
+		  		//insert css rules into stylesheet
 		      document.styleSheets[0].insertRule( keyframes, 0 );
 		      document.styleSheets[0].insertRule( after, 0 );
-		 
+
 		  } else {
-		 
+		 		//if no stylesheet, create one and insert css rules into stylesheet
 		    var s = document.createElement( 'style' );
 		    s.innerHTML = keyframes;
 		    document.getElementsByTagName( 'head' )[ 0 ].appendChild( s );
-		 
 		  }
-		 
-		// }
 
+		}
+
+
+		//set intial css according to init and dir
+		self.css({
+			'-webkit-transform': 'perspective( 600px ) rotateX( ' + initRot + 'deg )',
+			'-webkit-transform-origin': '50% 0%',
+			'-webkit-transition': 'opacity 1s ease-in'
+		});
+		 
 		//set ending css according to init and dir, triggered by event
 		self.on('click', function() {
 			self.addClass('after');
 		});
 
-
-		
-		//return self
-
+		return self
 	}
 
 })(jQuery);
